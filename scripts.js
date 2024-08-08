fdcf26cf7e349e6920ecc74f3080388d59c5464b
Vatsal Unadkat <31386890+vatsalunadkat@users.noreply.github.com>
2024-08-08 20:03:33 +0530

    Animated text + font + favicon

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Document is ready!');
+document.addEventListener("DOMContentLoaded", function() {
+    const roleElement = document.querySelector('.role');
+    const text = roleElement.textContent.trim();
+    const chars = text.split('');
+    const wrappedChars = chars.map(char => `<span>${char}</span>`).join('');
+    roleElement.innerHTML = wrappedChars;
+    roleElement.classList.add('anim-text-flow');
 });
